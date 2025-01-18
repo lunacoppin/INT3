0e1883dc8aeb3f11759f53868b13ce8105a1c3aa
lunacoppin <luna.coppin@student.howest.be>
2025-01-18 17:00:12 +0100

    hamburger navigation

---
+++ src/js/animations.js
@@ -0,0 +1,45 @@
+import { DotLottie } from '@lottiefiles/dotlottie-web';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+// const player = new DotLottie({
+//     canvas: document.querySelector(".animation"),
+//     src: "/lottie/dashes-square.json",
+// });
+
+// gsap.to(player.canvas, {
+//     scrollTrigger: {
+//         trigger: player.canvas,
+//         start: 'top top',
+//         end: '+=1000',
+//         pin: true,
+//         scrub: true,
+//         onUpdate: (self) => {
+//             player.setFrame(self.progress * (player.totalFrames - 1));
+//         }
+//     },
+// });
+
+const path = document.querySelector("#drawPath");
+
+// Get the length of the path
+const pathLength = path.getTotalLength();
+
+// Set initial strokeDasharray and strokeDashoffset
+path.style.strokeDasharray = "12, 12";
+path.style.strokeDashoffset = pathLength;
+
+// GSAP ScrollTrigger animation
+gsap.to(path, {
+    strokeDashoffset: 0, // Animate to a strokeDashoffset of 0
+    scrollTrigger: {
+        trigger: path,          // Trigger the animation when the path comes into view
+        start: "top bottom",       // When the top of the path reaches 80% of the viewport height
+        end: "bottom top-=3000",      // When the bottom of the path reaches 20% of the viewport height
+        scrub: true,            // Smoothly animate as you scroll
+        markers: false,           // Show the start and end markers for debugging
+    }
+});
+
---
+++ src/js/script.js
@@ -0,0 +1,50 @@
+const init = () => {
+    const $navButton = document.querySelector('.nav__button--menu');
+    const $navList = document.querySelector('.nav__list');
+    const $iconLink = document.querySelector('#iconlink');
+    const listItems = $navList.querySelectorAll("li a");
+
+    $navButton.classList.remove('hidden');
+    $navList.classList.add("hidden");
+
+    const openNavigation = () => {
+        $navButton.setAttribute("aria-expanded", "true");
+        $iconLink.setAttribute("xlink:href", "#close");
+        $navList.classList.remove("hidden");
+    }
+
+    const closeNavigation = () => {
+        $navButton.setAttribute("aria-expanded", "false");
+        $iconLink.setAttribute("xlink:href", "#navicon");
+        $navList.classList.add("hidden");
+    }
+
+    const toggleNavigation = () => {
+        const open = $navButton.getAttribute("aria-expanded");
+        open === "false" ? openNavigation() : closeNavigation();
+    }
+
+
+    const handleBlur = () => {
+        //if (!event.relatedTarget || !$navList.contains(event.relatedTarget)) {
+        closeNavigation();
+        //}
+    }
+
+    $navButton.addEventListener("click", toggleNavigation);
+
+    // add event to the last item in the nav list to trigger the disclosure to close if the user tabs out of the disclosure
+    listItems[listItems.length - 1].addEventListener("blur", handleBlur);
+
+    // Close the disclosure if a user presses the escape key
+    window.addEventListener("keyup", (e) => {
+        if (e.key === "Escape") {
+            $navButton.focus();
+            closeNavigation();
+        }
+    });
+}
+
+
+
+init();
