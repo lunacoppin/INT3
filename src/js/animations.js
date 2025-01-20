09887d759f560331aa069f5690764cea11416333
lunacoppin <luna.coppin@student.howest.be>
2025-01-20 12:00:26 +0100

    background paths animating

--- src/js/animations.js
+++ src/js/animations.js
@@ -22,26 +22,28 @@ gsap.registerPlugin(ScrollTrigger);
 //     },
 // });
 
-// const path = document.querySelector("#drawPath");
+const paths = document.querySelectorAll(".draw-path");
 
-// // Get the length of the path
-// const pathLength = path.getTotalLength();
+paths.forEach((path) => {
+    // Get the length of the path
+    const pathLength = path.getTotalLength();
 
-// // Set initial strokeDasharray and strokeDashoffset
-// path.style.strokeDasharray = "12, 12";
-// path.style.strokeDashoffset = pathLength;
+    // Set initial strokeDasharray and strokeDashoffset
+    path.style.strokeDasharray = "12, 12";
+    path.style.strokeDashoffset = pathLength;
 
-// // GSAP ScrollTrigger animation
-// gsap.to(path, {
-//     strokeDashoffset: 0, // Animate to a strokeDashoffset of 0
-//     scrollTrigger: {
-//         trigger: path,          // Trigger the animation when the path comes into view
-//         start: "top bottom",       // When the top of the path reaches 80% of the viewport height
-//         end: "bottom top-=3000",      // When the bottom of the path reaches 20% of the viewport height
-//         scrub: true,            // Smoothly animate as you scroll
-//         markers: false,           // Show the start and end markers for debugging
-//     }
-// });
+    // GSAP ScrollTrigger animation
+    gsap.to(path, {
+        strokeDashoffset: 0, // Animate to a strokeDashoffset of 0
+        scrollTrigger: {
+            trigger: path,          // Trigger the animation when the path comes into view
+            start: "top bottom",       // When the top of the path reaches 80% of the viewport height
+            end: "bottom top-=5000",      // When the bottom of the path reaches 20% of the viewport height
+            scrub: true,            // Smoothly animate as you scroll
+            markers: false,           // Show the start and end markers for debugging
+        }
+    });
+});
 
 const animateProgress = () => {
     const progressBar = document.querySelector(".progress--fg");
